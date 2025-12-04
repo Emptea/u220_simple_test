b40272f0dfcbb9a63f671fb19eb68f8ed84d822e
Emptea <aryas1725@gmail.com>
2025-12-04 13:44:18 +0300

    changed sg generation to vecgtor

--- test_example.py
+++ test_example.py
@@ -32,8 +32,6 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import test_example_epy_block_0 as epy_block_0  # embedded python block
-import test_example_epy_block_0_0 as epy_block_0_0  # embedded python block
 
 
 
@@ -78,60 +76,13 @@ class test_example(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = 5e6
         self.train_freq = train_freq = samp_rate/232
         self.gain = gain = 75
+        self.duty = duty = 40/232
         self.center_freq = center_freq = 3.6e9
         self.bw = bw = 56e6
 
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_time_sink_x_0_0_0_0_0 = qtgui.time_sink_f(
-            232, #size
-            samp_rate, #samp_rate
-            "Generated signal", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_time_sink_x_0_0_0_0_0.set_update_time(0.1)
-        self.qtgui_time_sink_x_0_0_0_0_0.set_y_axis(-1, 1)
-
-        self.qtgui_time_sink_x_0_0_0_0_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0_0_0_0_0.enable_tags(True)
-        self.qtgui_time_sink_x_0_0_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_AUTO, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0_0_0_0_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0_0_0_0_0.enable_grid(True)
-        self.qtgui_time_sink_x_0_0_0_0_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0_0_0_0_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0_0_0_0_0.enable_stem_plot(False)
-
-
-        labels = ['re', 'im', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0_0_0_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0_0_0_0_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0_0_0_0_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0_0_0_0_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0_0_0_0_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0_0_0_0_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0_0_0_0_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_0_0_0_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0_0_0_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_0_0_0_win)
         self.qtgui_time_sink_x_0_0_0_0 = qtgui.time_sink_f(
             232, #size
             samp_rate, #samp_rate
@@ -180,20 +131,13 @@ class test_example(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_0_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0_0_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_0_0_win)
-        self.epy_block_0_0 = epy_block_0_0.blk(sample_rate=samp_rate, frequency=train_freq, duty_cycle=6/232)
-        self.epy_block_0 = epy_block_0.blk(sample_rate=samp_rate, frequency=train_freq, duty_cycle=40/232)
-        self.blocks_delay_0 = blocks.delay(gr.sizeof_float*1, 141)
-        self.blocks_add_xx_0 = blocks.add_vff(1)
+        self.blocks_vector_source_x_0 = blocks.vector_source_f([1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1 ,1 ,1 ,1 ,1 ,1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0], True, 1, )
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_time_sink_x_0_0_0_0, 0))
-        self.connect((self.blocks_delay_0, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.epy_block_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0_0_0_0_0, 0))
-        self.connect((self.epy_block_0_0, 0), (self.blocks_delay_0, 0))
+        self.connect((self.blocks_vector_source_x_0, 0), (self.qtgui_time_sink_x_0_0_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -210,24 +154,25 @@ class test_example(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.set_train_freq(self.samp_rate/232)
-        self.epy_block_0.sample_rate = self.samp_rate
-        self.epy_block_0_0.sample_rate = self.samp_rate
         self.qtgui_time_sink_x_0_0_0_0.set_samp_rate(self.samp_rate)
-        self.qtgui_time_sink_x_0_0_0_0_0.set_samp_rate(self.samp_rate)
 
     def get_train_freq(self):
         return self.train_freq
 
     def set_train_freq(self, train_freq):
         self.train_freq = train_freq
-        self.epy_block_0.frequency = self.train_freq
-        self.epy_block_0_0.frequency = self.train_freq
 
     def get_gain(self):
         return self.gain
 
     def set_gain(self, gain):
         self.gain = gain
+
+    def get_duty(self):
+        return self.duty
+
+    def set_duty(self, duty):
+        self.duty = duty
 
     def get_center_freq(self):
         return self.center_freq
